9d1a8b1cbbc21cc6d209456a56bc3c694fbbc29d
ericong18 <ericong18@gmail.com>
2019-02-21 00:48:31 -0800

    Setup route for emails

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,31 @@
 const express = require('express')
 const cors = require('cors')
-const sendgrid = require('@sendgrid/mail')
+const sgMail = require('@sendgrid/mail')
 const app = express()
 const port = process.env.PORT || 8080
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-  // const msg = {
-  //   to: 'test@example.com',
-  //   from: 'test@example.com',
-  //   subject: 'Sending with SendGrid is Fun',
-  //   text: 'and easy to do anywhere, even with Node.js',
-  //   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  // };
 
   res.send("Hello world")
+})
+
+app.get('/email', (req, res) => {
+  const msg = {
+    to: 'ericong18@gmail.com',
+    from: 'test@example.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+
+  sgMail.send(msg)
+  .then(res => console.log(res))
+
+  res.send("Email sent via Sendgrid")
 })
 
 app.listen(port, () => console.log("Listening on port " + port))
