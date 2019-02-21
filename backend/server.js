9841401e02546b81a07cabd0ae0143a8b7699715
ericong18 <ericong18@gmail.com>
2019-02-21 00:33:30 -0800

    Successfully deployed express server on heroku

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,22 @@ const express = require('express')
 const cors = require('cors')
 const sendgrid = require('@sendgrid/mail')
 const app = express()
-const port = 8080
-const keys = require('./config/keys')
+const port = process.env.PORT || 8080
 
-sendgrid.setApiKey(keys.sendgridKey)
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-  const msg = {
-    to: 'test@example.com',
-    from: 'test@example.com',
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  };
+  // const msg = {
+  //   to: 'test@example.com',
+  //   from: 'test@example.com',
+  //   subject: 'Sending with SendGrid is Fun',
+  //   text: 'and easy to do anywhere, even with Node.js',
+  //   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  // };
 
-  
+  res.send("Hello world")
 })
 
 app.listen(port, () => console.log("Listening on port " + port))
